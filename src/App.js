6f36b228a6bd47c88169cd29a135ab4f18c5b8f4
garfield-spst <94508945+garfield-spst@users.noreply.github.com>
2022-07-15 18:07:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
+import NotFound from "./components/NotFound";
+import React from "react";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
